7c34bc524b916cb34d96316560e9fe57725b4709
MuraliVelayudam <murali.velayudam@gmail.com>
2024-04-14 17:21:35 +0530

    Auth Updated

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 
